d5780da8b9aa7ec538d21168665346bf6ab0515b
Leo Carnovale <Leo.carnovale@gmail.com>
2019-10-13 12:53:33 +1100

    Rearranged templates and added routes

--- routes.py
+++ routes.py
@@ -6,8 +6,20 @@ from server import app
 Landing page
 '''
 @app.route('/', methods=['GET', 'POST'])
-def landing():
+def home():
     if request.method == 'POST':
         pass
 
-    return render_template('index.html')
+    return render_template('base.html')
+
+
+'''
+Main Booking page
+'''  
+@app.route('/book', methods=['GET', 'POST'])
+def book_main():
+    if request.method == 'POST':
+        pass
+
+    return render_template('book.html')
+
