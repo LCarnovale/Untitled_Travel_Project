1f626ea438762d82e32aa7123e6d4a25770d74cd
Leo Carnovale <Leo.carnovale@gmail.com>
2019-10-15 12:39:55 +1100

    Connected to databse and executed simple SQL query.

---
+++ cloud/testQuery.sql
@@ -0,0 +1,6 @@
+SELECT * FROM dbo.dbTable
+
+INSERT INTO dbo.dbTable (UserID, Name)
+VALUES (5, 'Ian')
+
+SELECT * FROM dbo.dbTable
---
+++ cloud/test_deploy.py
@@ -0,0 +1,46 @@
+from azure.mgmt.web import WebSiteManagementClient
+from azure.mgmt.web.models import Site, SiteSourceControl, SiteConfig
+from azure.common.client_factory import get_client_from_cli_profile
+
+RESOURCE_GROUP_NAME = 'testGroup'
+SERVICE_PLAN_NAME = 'testPlan'
+WEB_APP_NAME = 'sampleflaskapp123'
+REPO_URL = 'https://github.com/comp3300-comp9900-term-3-2019/capstone-project-untitled'
+
+#log in
+web_client = get_client_from_cli_profile(WebSiteManagementClient)
+
+# get service plan id
+service_plan = web_client.app_service_plans.get(
+    RESOURCE_GROUP_NAME, SERVICE_PLAN_NAME)
+
+# create a web app
+siteConfiguration = SiteConfig(
+    python_version='3.4',
+)
+site_async_operation = web_client.web_apps.create_or_update(
+    RESOURCE_GROUP_NAME,
+    WEB_APP_NAME,
+    Site(
+        location='australiaeast',
+        server_farm_id=service_plan.id,
+        site_config=siteConfiguration
+    ),
+)
+
+site = site_async_operation.result()
+print('created webapp: ' + site.default_host_name)
+
+# continuous deployment with GitHub
+source_control_async_operation = web_client.web_apps.create_or_update_source_control(
+    RESOURCE_GROUP_NAME,
+    WEB_APP_NAME,
+    SiteSourceControl(
+        is_mercurial=False,
+        repo_url=REPO_URL,
+        branch='master'
+    )
+)
+
+source_control = source_control_async_operation.result()
+print("added source control to: " + source_control.name + "azurewebsites.net")
