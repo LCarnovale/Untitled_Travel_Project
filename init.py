5f7ccc57d8d5c96246740137180fff97dbde776a
Leo Carnovale <Leo.carnovale@gmail.com>
2019-10-20 22:41:22 +1100

    updating requirements to attempt to fix app deployment

--- init.py
+++ init.py
@@ -7,12 +7,12 @@ def bootstrap_system():
     db.init()
     
     # Debug: Get the latest commit
-    f = open(".git/HEAD", 'r')
-    com = f.read()
-    f.close()
-    if 'ref:' in com:
-        f = open(".git/" + com[5:-1], 'r')
-        com = f.read()
-        f.close()
+    # f = open(".git/HEAD", 'r')
+    # com = f.read()
+    # f.close()
+    # if 'ref:' in com:
+    #     f = open(".git/" + com[5:-1], 'r')
+    #     com = f.read()
+    #     f.close()
     
-    server.DBG_current_commit = com
+    # server.DBG_current_commit = com
